d9907ece22838f13a34d0a8effc0db21ecd50bd8
vincent <vpark45@gmail.com>
2024-03-30 18:43:22 +0900

    getByteString을 skip으로 변경

--- src/OLE.hs
+++ src/OLE.hs
@@ -25,13 +25,13 @@ instance Binary HeaderBlock where
     if magic /= "\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1"
       then fail "Invalid Magic ID"
       else do
-        getByteString 36
+        skip 36
         bbatNum <- getWord32le
         property <- getWord32le
-        getByteString 8
+        skip 8
         sbatStart <- getWord32le
         sbatNum <- getWord32le
-        getByteString 8
+        skip 8
         members <- sequence (replicate (fromIntegral bbatNum) getWord32le)
         return
           (HeaderBlock magic bbatNum property sbatStart sbatNum members)
